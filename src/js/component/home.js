f8ff764c59054ed84eeb80f1bfb672279ae2622c
Victoria Dezerega <videzere@gmail.com>
2021-04-15 14:41:44 -0400

    Actualizar GitHud v1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [segundos, setSegundos] = useState(0);
+	useEffect(() => {
+		let idInterval = setInterval(
+			() => setSegundos(segundosPrev => segundosPrev + 1),
+			300
+		);
+	}, []);
+	// clearInterval(idInterval);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container className="bg-dark text-white">
+			<Row className="justify-content-center">
+				<Col className="p-2" xs={2}>
+					<i className="fas fa-clock"></i>
+				</Col>
+				<Col className="p-2" xs={1}>
+					{Math.floor(segundos / 100000) % 10}
+				</Col>
+				<Col className="p-2" xs={1}>
+					{Math.floor(segundos / 10000) % 10}
+				</Col>
+				<Col className="p-2" xs={1}>
+					{Math.floor(segundos / 1000) % 10}
+				</Col>
+				<Col className="p-2" xs={1}>
+					{Math.floor(segundos / 100) % 10}
+				</Col>
+				<Col className="p-2" xs={1}>
+					{Math.floor(segundos / 10) % 10}
+				</Col>
+				<Col className="p-2" xs={1}>
+					{segundos % 10}
+				</Col>
+			</Row>
+		</Container>
 	);
 }
