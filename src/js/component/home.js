98e10cc98de56a9363e946175d5f66b12ea16c48
Victoria Dezerega <videzere@gmail.com>
2021-05-19 17:08:40 -0400

    Actualizar GitHud v3

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
@@ -9,32 +7,32 @@ export function Home() {
 	useEffect(() => {
 		let idInterval = setInterval(
 			() => setSegundos(segundosPrev => segundosPrev + 1),
-			300
+			1000
 		);
 	}, []);
 	// clearInterval(idInterval);
 	return (
-		<Container className="bg-dark text-white">
-			<Row className="justify-content-center">
-				<Col className="p-2" xs={2}>
-					<i className="fas fa-clock"></i>
+		<Container className="contstyle text-white">
+			<Row className="d-flex flex-row justify-content-center p-2">
+				<Col className="colstyle" xs={2}>
+					<i className="m-auto text-white far fa-clock fa-flip-horizontal"></i>
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{Math.floor(segundos / 100000) % 10}
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{Math.floor(segundos / 10000) % 10}
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{Math.floor(segundos / 1000) % 10}
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{Math.floor(segundos / 100) % 10}
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{Math.floor(segundos / 10) % 10}
 				</Col>
-				<Col className="p-2" xs={1}>
+				<Col className="colstyle" xs={1}>
 					{segundos % 10}
 				</Col>
 			</Row>
